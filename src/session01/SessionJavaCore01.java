f3ba3cc6f868275f01e2b70ece69bd17c43261e4
origin <thuynguyenminh0303@gmail.com>
2023-02-25 23:08:12 +0700

    JC_01

---
+++ src/session01/SessionJavaCore01.java
@@ -0,0 +1,11 @@
+package session01;
+
+public class SessionJavaCore01 {
+public static void main(String[] args) {
+	System.out.println("Chin chào thúy đến với bài Java core đầu tiên");
+	System.out.println("Họ tên: Nguyễn Công Minh Thúy");
+	System.out.println("Năm sinh: 2003, 19 tuổi");
+	System.out.println("Quê quán: Đông Anh, Hà Nội");
+	System.out.println("Giới tính: Nữ, Học lớp c2110h2");
+}
+}
